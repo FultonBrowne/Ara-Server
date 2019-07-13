38dd3ade57da3da79a9088e724c7ae27970eecc7
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-13 12:22:21 -0700

    added stuff

--- src/main/java/com/andromeda/araserver/main.java
+++ src/main/java/com/andromeda/araserver/main.java
@@ -1,7 +1,33 @@
 package com.andromeda.araserver;
 
+import org.graalvm.compiler.asm.sparc.SPARCAssembler;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class main {
+    public static String version = "ara 0.1 in progress ";
+
     public static void main (String args[]){
         System.out.println("Starting Ara");
+        System.out.println(version);
+        System.out.println("RSS server URL or IP");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String mInputUrl = null;
+        try {
+            mInputUrl = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(mInputUrl);
+        try {
+            URL url = new URL(mInputUrl) ;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
     }
 }
