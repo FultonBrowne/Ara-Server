fbefebcfd22583d61fa95f1338657157ff845d62
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-07-14 13:35:37 -0700

    added stuff again

--- src/main/java/com/andromeda/araserver/main.java
+++ src/main/java/com/andromeda/araserver/main.java
@@ -5,16 +5,28 @@ import org.graalvm.compiler.asm.sparc.SPARCAssembler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.UnknownHostException;
 
 public class main {
     public static String version = "ara 0.1 in progress ";
 
     public static void main (String args[]){
+        InetAddress mIP;
+        String mIP2;
+
         System.out.println("Starting Ara");
         System.out.println(version);
         System.out.println("RSS server URL or IP");
+        try {
+          mIP  = InetAddress.getLocalHost();
+          mIP2 = mIP.toString();
+          System.out.println(mIP2);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String mInputUrl = null;
         try {
