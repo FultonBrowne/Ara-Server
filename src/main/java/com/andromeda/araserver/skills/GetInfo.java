5b4f2a338b39867cf432c56c5ea7bf4a1fe8992d
FultonB <50185337+andromeda-software@users.noreply.github.com>
2019-10-11 16:08:31 -0700

    testing and testing

--- src/main/java/com/andromeda/araserver/skills/GetInfo.java
+++ src/main/java/com/andromeda/araserver/skills/GetInfo.java
@@ -29,6 +29,11 @@ public class GetInfo {
         ArrayList<String> pairs = new ArrayList<>(Arrays.asList(mainurl.split("&")));
         term = pairs.get(0);
         System.out.println(term);
+        try {
+            getFast("ten times ten");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         outputModels.add(new OutputModel("test", mainurl, "https://github.com/FultonBrowne/Ara-Server", "", "", ""));
         try {
             outputModels.addAll(searchBing(term));
@@ -55,16 +60,15 @@ public class GetInfo {
         JsonObject jsonObject = jelement.getAsJsonObject();
         jsonObject = jsonObject.getAsJsonObject("webPages");
         JsonArray jsonArray = jsonObject.getAsJsonArray("value");
-        System.out.println(jsonArray.size());
         getFast("two times two");
         for (int i = 0; i < jsonArray.size(); i++) {
-            System.out.println(jsonArray.get(i).isJsonObject());
+            //System.out.println(jsonArray.get(i).isJsonObject());
             String title = jsonArray.get(i).getAsJsonObject().get("name").getAsString();
-            System.out.println(title);
+            //System.out.println(title);
             String info = jsonArray.get(i).getAsJsonObject().get("snippet").getAsString();
             System.out.println(info);
             String link = jsonArray.get(i).getAsJsonObject().get("url").getAsString();
-            System.out.println(link);
+            //System.out.println(link);
             mainList.add(new OutputModel(title, info, link, "", "", ""));
 
 
@@ -86,7 +90,7 @@ public class GetInfo {
         String response = new Scanner(stream).useDelimiter("\\A").next();
         JsonElement jelement = new JsonParser().parse(response);
         JsonObject jsonObject = jelement.getAsJsonObject();
-        jsonObject = jsonObject.getAsJsonObject("expression");
+        jsonObject = jsonObject.getAsJsonObject("Computation");
         System.out.println(jsonObject);
         //JsonArray jsonArray = jsonObject.getAsJsonArray("value");
 
