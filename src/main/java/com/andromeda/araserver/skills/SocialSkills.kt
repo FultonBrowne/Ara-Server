1a5405379cdc65a87f8d098127be658b15f3ec67
fulton <gprlabs@outlook.com>
2019-11-01 10:15:39 -0700

    adding stuff for ara skills

---
+++ src/main/java/com/andromeda/araserver/skills/SocialSkills.kt
@@ -0,0 +1,13 @@
+package com.andromeda.araserver.skills
+//this is where ara is nice to people :)
+class SocialSkills {
+    private fun doYouLike(thing:String){
+        when (thing){
+            "star trek"-> TODO()
+            "star wars"-> TODO()
+        }
+    }
+    fun mainFun(){
+
+    }
+}
