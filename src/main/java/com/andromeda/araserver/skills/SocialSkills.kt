9da4369eaa8c5002a52eecf42aa25210c8aeda50
fulton <gprlabs@outlook.com>
2019-11-02 16:15:19 -0700

    added some personality code

--- src/main/java/com/andromeda/araserver/skills/SocialSkills.kt
+++ src/main/java/com/andromeda/araserver/skills/SocialSkills.kt
@@ -1,17 +1,22 @@
 package com.andromeda.araserver.skills
+
+import com.andromeda.araserver.util.OutputModel
+
 //this is where ara is nice to people :)
 class SocialSkills {
     fun doYouLike(thing:String){
+        var returedVal = OutputModel("", "", "", "", "", "")
         when (thing){
-            "star trek"-> TODO()
-            "star wars"-> TODO()
-            "google"-> TODO()
+            "star trek"-> returedVal = OutputModel("Yes", "I love next generation, Data is my favorite", "", "", "Yes, I love next generation, Data is my favorite", "")
+            "star wars"-> returedVal = OutputModel("Yes", "I love Star Wars, my favorite is the one is space", "", "", "Love star wars I do", "")
+            "google"-> returedVal = OutputModel("Yup", "The google assistant is a friend of mine", "", "", "yup, The google assistant is a friend of mine", "")
         }
     }
     fun mainFun(){
 
     }
     fun favorite(thing: String){
+        var returedVal = OutputModel("", "", "", "", "", "")
         when (thing){
             "colour" -> TODO()
         }
