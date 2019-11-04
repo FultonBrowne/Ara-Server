2fa2edd7070d1fd253545ef8166cd71008657749
fulton <gprlabs@outlook.com>
2019-11-04 08:23:31 -0800

    added lat/log parser to Weather.kt

--- src/main/java/com/andromeda/araserver/skills/Weather.kt
+++ src/main/java/com/andromeda/araserver/skills/Weather.kt
@@ -1,7 +1,23 @@
 package com.andromeda.araserver.skills
 
+import java.util.*
+
 class Weather {
+    private var log: String? = null
+    private var lat: String? = null
+    private var term: String? = null
     fun getWeather(url:String){
+        //get api params
+        val pairs =
+            ArrayList(listOf(*url.split("&".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()))
+        //Finish the job an get the raw values
+        for (pair in pairs) {
+            when {
+                pair.startsWith("log") -> log = pair.replace("log=", "")
+                pair.startsWith("lat") -> lat = pair.replace("lat=", "")
+                else -> term = pair.replace("/yelpclient/", "")
+            }
+        }
 
     }
 }
