82bd7c42f8a8cfc19d42eea16154510e9fb9851f
fulton <gprlabs@outlook.com>
2019-11-13 15:51:35 -0600

    parse framework thing

--- src/main/java/com/andromeda/araserver/util/KeyWord.kt
+++ src/main/java/com/andromeda/araserver/util/KeyWord.kt
@@ -1,5 +1,26 @@
 package com.andromeda.araserver.util
 
-class KeyWord {
+import opennlp.tools.cmdline.parser.ParserTool
+import opennlp.tools.langdetect.LanguageDetectorModel
+import opennlp.tools.parser.Parse
+import opennlp.tools.parser.ParserFactory
+import opennlp.tools.parser.ParserModel
+import java.io.FileInputStream
+import java.io.InputStream
+
+
+class KeyWord(text:String, is1:InputStream ) {
+    private val mainVal = text
+    private val mainIs = is1
+    fun getKeyWords(): Array<out Parse>? {
+        val m =ParserModel(mainIs)
+        val parser = ParserFactory.create(m)
+
+
+        return ParserTool.parseLine(mainVal, parser, 1)
+    }
+
+
+
 
 }
