69c33ac53f14827787188dd58e802d54fcd4bbab
fulton <gprlabs@outlook.com>
2019-11-16 14:11:42 -0600

    about done now need to add it to search

--- src/main/java/com/andromeda/araserver/util/SortWords.kt
+++ src/main/java/com/andromeda/araserver/util/SortWords.kt
@@ -4,9 +4,9 @@ import opennlp.tools.parser.Parse
 import java.util.ArrayList
 
 class SortWords(keyWord: KeyWord, mainVal: String) {
-    val key = keyWord
-    val mainText = mainVal
-    fun getTopics() {
+    private val key = keyWord
+    private val mainText = mainVal
+    fun getTopics(): ArrayList<WordGraph> {
         var toReturn = ArrayList<WordGraph>()
 
         var graph = key.getKeyWords(mainText)?.get(0)
@@ -16,33 +16,38 @@ class SortWords(keyWord: KeyWord, mainVal: String) {
 
             while (working) {
                 when {
-                    graph?.childCount == 1 -> {graph = graph.children?.get(0)
-                    print(graph?.type)}
+                    graph?.childCount == 1 -> {
+                        graph = graph.children?.get(0)
+                    }
                     graph?.childCount == 0 -> {
                         working = false
                     }
                     else -> {
                         if (graph != null) {
-                            sortForGraph(graph)
+                            toReturn.addAll(sortForNoun(graph))
                         }
                         working = false
-                        }
+                    }
 
                 }
             }
         } else {
             println("fail")
         }
+        return toReturn
 
 
     }
-    private fun sortForGraph(graph: Parse): ArrayList<WordGraph> {
+
+    private fun sortForNoun(graph: Parse): ArrayList<WordGraph> {
         val toReturn = ArrayList<WordGraph>()
         for (i in graph.children!!) {
-            toReturn.add(WordGraph(i.coveredText, i.label))
-            println(i.type)
-            if (i.childCount > 0) toReturn.addAll(sortForGraph(i))
+            if (i.type == "NN") {
+                toReturn.add(WordGraph(i.coveredText, i.type))
+                //println("add")
             }
+            if (i.childCount > 0) toReturn.addAll(sortForNoun(i))
+        }
         return toReturn
 
     }
