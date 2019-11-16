35a64be58748895bf6412ad180efe9545d79e90a
fulton <gprlabs@outlook.com>
2019-11-16 12:31:40 -0600

    added TOP detector

--- src/main/java/com/andromeda/araserver/util/SortWords.kt
+++ src/main/java/com/andromeda/araserver/util/SortWords.kt
@@ -4,9 +4,15 @@ class SortWords(keyWord: KeyWord, mainVal:String) {
     val key = keyWord
     val mainText = mainVal
     fun getTopics(){
-        val graph = key.getKeyWords(mainText)?.get(0)
+        var graph = key.getKeyWords(mainText)?.get(0)
+        var working = true
         if (graph != null) {
-            graph.label
+
+            while (working){
+                if (graph?.label == "TOP") graph = graph.children?.get(0)
+                else working = false
+                graph?.show()
+            }
         }
         else{
             println("fail")
