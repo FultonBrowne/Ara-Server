a8e6f9c585dccbaddc169cce43db389182d27586
fulton <gprlabs@outlook.com>
2019-11-16 13:42:26 -0600

    keywords almost done

--- src/main/java/com/andromeda/araserver/util/SortWords.kt
+++ src/main/java/com/andromeda/araserver/util/SortWords.kt
@@ -1,10 +1,13 @@
 package com.andromeda.araserver.util
 
+import opennlp.tools.parser.Parse
+
 class SortWords(keyWord: KeyWord, mainVal: String) {
     val key = keyWord
     val mainText = mainVal
     fun getTopics() {
         var graph = key.getKeyWords(mainText)?.get(0)
+
         var working = true
         if (graph != null) {
 
@@ -12,24 +15,34 @@ class SortWords(keyWord: KeyWord, mainVal: String) {
                 when {
                     graph?.childCount == 1 -> {graph = graph.children?.get(0)
                     println("is 1")
-                    print(graph?.label)}
+                    print(graph?.type)}
                     graph?.childCount == 0 -> {
                         working = false
                         println("stop")
                     }
                     else -> {
-                        for (i in graph?.children!!) {
-                            println("go 2")
-                            println(i.label)
+                        if (graph != null) {
+                            sort(graph)
                         }
                         working = false
-                    }
+                        }
+
                 }
             }
         } else {
             println("fail")
         }
 
+
+    }
+    private fun sort(graph: Parse){
+        for (i in graph.children!!) {
+            //println("go 2")
+            println(i.type)
+            if (i.childCount > 0) sort(i)
+
+
+            }
 
     }
 
