be2628976bb5b8b65de62816bb907ef3b22d8c61
fulton <gprlabs@outlook.com>
2019-11-17 19:13:51 -0600

    test things

---
+++ src/test/java/com/andromeda/araserver/test/NLPtests.java
@@ -0,0 +1,18 @@
+package com.andromeda.araserver.test;
+
+import com.andromeda.araserver.util.KeyWord;
+import org.junit.Test;
+
+import java.io.InputStream;
+
+
+public class NLPtests {
+    @Test
+    public void mainTest(){
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("parse.bin");
+        new KeyWord(is).getKeyWords("Hello unit test")[0].show();
+
+    }
+
+}
