ed5e4df78bb06ba876cdbbb2e4d76eedf9ea5acd
fulton <gprlabs@outlook.com>
2019-11-19 07:04:24 -0500

    experiments with static in kotlin

--- src/main/java/com/andromeda/araserver/util/KeyWord.kt
+++ src/main/java/com/andromeda/araserver/util/KeyWord.kt
@@ -9,19 +9,16 @@ import java.io.InputStream
 
 
 class KeyWord(parseIS:InputStream) {
-    val parse = parseIS
-
-     var parser:Parser? = null
+   final val parse = parseIS
+    val m =ParserModel(parse)
+    val parser = ParserFactory.create(m)
 
 
     fun getKeyWords(mainVal:String): Array<out Parse>? {
 
         return ParserTool.parseLine(mainVal, parser, 1)
     }
-    init {
-       val m =ParserModel(parse)
-        parser = ParserFactory.create(m)
-    }
+
 
     }
 
