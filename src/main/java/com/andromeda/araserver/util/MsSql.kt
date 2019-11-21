ff1dd6f490af30cf3644d7d0d60a664cb9366db9
fulton <gprlabs@outlook.com>
2019-11-21 07:19:50 -0500

    connection crap

--- src/main/java/com/andromeda/araserver/util/MsSql.kt
+++ src/main/java/com/andromeda/araserver/util/MsSql.kt
@@ -1,5 +1,25 @@
 package com.andromeda.araserver.util
 
+import java.sql.Connection
+import java.sql.DriverManager
+
 class MsSql {
-    fun getSkills(){}
+    private val link = "araresdb.database.windows.net"
+    private val userName = "pholtor"
+    private val password =  System.getenv("passWord");
+    var url = String.format(
+        "jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;" + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
+        link,
+        "ara",
+        userName,
+        password
+    )
+    fun getSkills(): String {
+        var connection: Connection? = null
+        val link = "test"
+        connection = DriverManager.getConnection(url);
+
+        return link
+
+    }
 }
