d67e01c1e55e047abf14c6caf3223f2ef575c6bf
fulton <gprlabs@outlook.com>
2019-12-21 08:17:46 -0800

    done with skills support server

--- src/main/java/com/andromeda/araserver/pages/GetSkillData.kt
+++ src/main/java/com/andromeda/araserver/pages/GetSkillData.kt
@@ -2,8 +2,8 @@ package com.andromeda.araserver.pages
 
 class GetSkillData {
     fun main(url:String): String? {
-        val mainVal = url.replace("/skilldata/", "")
-        val mainMap = mapOf("CALL" to "to call", "TOG_MEDIA" to "", "SITE" to "what site?","OUTPUT" to "title and description", "TEXT" to "Who to text?", "OPEN_APP" to "what app I run?")
+        val mainVal = url.replace("/skillsdata/", "")
+        val mainMap = mapOf("CALL" to "who should I call?", "TOG_MEDIA" to "", "SITE" to "what site?","OUTPUT" to "title and description", "TEXT" to "Who to text?", "OPEN_APP" to "what app I run?")
         return mainMap[mainVal]
     }
 }
