df92a4084837ebb5af4c81464e83c50da8480506
fulton <gprlabs@outlook.com>
2019-12-21 07:33:04 -0800

    working on a skills support server

---
+++ src/main/java/com/andromeda/araserver/pages/GetSkillData.kt
@@ -0,0 +1,7 @@
+package com.andromeda.araserver.pages
+
+class GetSkillData {
+    fun main(){
+        val mainMap = mapOf("CALL" to "to call", "TOG_MEDIA" to "", "SITE" to "what site?","OUTPUT" to "title and description", "TEXT" to "Who to text?")
+    }
+}
