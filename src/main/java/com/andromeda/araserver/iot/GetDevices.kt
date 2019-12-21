8a8853cb940dcfba2a2efa42947e42e207bdf977
fulton <gprlabs@outlook.com>
2019-12-21 12:31:57 -0800

    working on IOT

--- src/main/java/com/andromeda/araserver/iot/GetDevices.kt
+++ src/main/java/com/andromeda/araserver/iot/GetDevices.kt
@@ -4,22 +4,21 @@ import com.andromeda.araserver.util.DeviceModel
 import com.microsoft.azure.documentdb.Document
 import com.microsoft.azure.documentdb.DocumentClient
 import com.microsoft.azure.documentdb.FeedResponse
-
-
+import java.util.ArrayList
 
 
 class GetDevices {
-    fun main(client: DocumentClient, id:String){
+    fun main(client: DocumentClient, id:String): ArrayList<DeviceModel> {
+        val devices = ArrayList<DeviceModel>()
         val queryResults: FeedResponse<Document> = client.queryDocuments(
             "/dbs/Ara-android-database/colls/Ara-android-collection",
             "SELECT * FROM c WHERE c.id = $id",
             null
         )
         for (i in queryResults.queryIterator){
-
+            val deviceModel = DeviceModel(i["name"].toString(), i["type"].toString(), i["status"].toString(), i["group"].toString())
+            devices.add(deviceModel)
         }
-
-
-
+        return devices
     }
 }
