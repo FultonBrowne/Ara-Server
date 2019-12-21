f870fc6a26434cb27c516d34bd2039958d6bb94b
fulton <gprlabs@outlook.com>
2019-12-21 12:04:28 -0800

    working on IOT

--- src/main/java/com/andromeda/araserver/iot/Main.kt
+++ src/main/java/com/andromeda/araserver/iot/Main.kt
@@ -1,8 +1,33 @@
 package com.andromeda.araserver.iot
 
+import com.microsoft.azure.documentdb.ConnectionPolicy
+import com.microsoft.azure.documentdb.ConsistencyLevel
+import com.microsoft.azure.documentdb.DocumentClient
+
+
 class Main {
     fun main(url:String){
         val mainVal = url.replace("/devices/", "")
+        val actions = mainVal.split("&")
+        val dbLink = System.getenv("IOTDB")
+        var id:String? = null
+        var action:String? = null
+        var client = DocumentClient(
+            "https://ara-account-data.documents.azure.com:443/",
+            dbLink,
+             ConnectionPolicy(),
+            ConsistencyLevel.Session
+        )
+
+        for (i in actions){
+            when {
+                i.startsWith("id=") -> id = i.replace("id=", "")
+                i.startsWith("run=") -> action = i.replace("run=", "")
+                else -> throw SecurityException("not a valid set of arguments")
+            }
+        }
+
+
 
     }
 }
