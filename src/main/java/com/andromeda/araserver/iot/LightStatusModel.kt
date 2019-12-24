8a3a74124797de67aafa36eaed7bb19cc6ed3e88
fulton <gprlabs@outlook.com>
2019-12-24 14:32:29 -0800

    trying to make this WORK

--- src/main/java/com/andromeda/araserver/iot/LightStatusModel.kt
+++ src/main/java/com/andromeda/araserver/iot/LightStatusModel.kt
@@ -2,4 +2,3 @@ package com.andromeda.araserver.iot
 
 data class LightStatusModel(var on:Boolean, var powerLevel: Int?, var color:Int?)
 
-
--- src/main/java/com/andromeda/araserver/iot/WriteNewVal.kt
+++ src/main/java/com/andromeda/araserver/iot/WriteNewVal.kt
@@ -1,6 +1,7 @@
 package com.andromeda.araserver.iot
 
 import kotlin.reflect.KClass
+import kotlin.reflect.full.memberFunctions
 
 class WriteNewVal {
     fun main(classToMod: KClass<Any>, actionPair: List<String>?, any: Any) {
@@ -8,8 +9,9 @@ class WriteNewVal {
             if (member.name == actionPair!![0]) {
                 println(member.call(any))
                 println(member.parameters)
-            }
 
+                println(member)
+            }
 
         }
     }
