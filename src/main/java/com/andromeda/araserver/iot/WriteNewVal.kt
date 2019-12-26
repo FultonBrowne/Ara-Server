08c8b98861b52237c4f7c3af9d40edba1e79a18e
fulton <gprlabs@outlook.com>
2019-12-26 14:41:48 -0800

    got re set of class vals to work

--- src/main/java/com/andromeda/araserver/iot/WriteNewVal.kt
+++ src/main/java/com/andromeda/araserver/iot/WriteNewVal.kt
@@ -1,14 +1,17 @@
 package com.andromeda.araserver.iot
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KMutableProperty
 import kotlin.reflect.full.memberProperties
 
 class WriteNewVal {
     fun main(classToMod: KClass<Any>, actionPair: List<String>?, any: Any) {
-        classToMod.memberProperties.forEach { member ->
+        classToMod.memberProperties.filterIsInstance<KMutableProperty<*>>()
+            .forEach { member ->
             if (member.name == actionPair!![0]) {
                 println(member.name)
-                println(member.call(any, true))
+                println(member.setter.call(any, false))
+                println(member.getter.call(any))
 
             }
 
