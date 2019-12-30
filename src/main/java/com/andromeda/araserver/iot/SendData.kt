4010387bbf2b50e1c51009a1b40d86f2737c0568
fulton <gprlabs@outlook.com>
2019-12-30 14:23:29 -0800

    working on iot device status change message sender (is that a word?)

--- src/main/java/com/andromeda/araserver/iot/SendData.kt
+++ src/main/java/com/andromeda/araserver/iot/SendData.kt
@@ -1,5 +1,34 @@
 package com.andromeda.araserver.iot
 
-class SendData {
+import com.microsoft.azure.sdk.iot.service.*
 
+
+class SendData {
+    private val protocol = IotHubServiceClientProtocol.AMQPS
+
+    fun main(connectionString: String, deviceId:String){
+        val serviceClient = ServiceClient.createFromConnectionString(
+            connectionString, protocol
+        )
+
+        if (serviceClient != null) {
+            serviceClient.open()
+            val feedbackReceiver = serviceClient
+                .feedbackReceiver
+            feedbackReceiver?.open()
+            val messageToSend = Message("Cloud to device message.")
+            messageToSend.deliveryAcknowledgement = DeliveryAcknowledgement.Full
+            serviceClient.send(deviceId, messageToSend)
+            println("Message sent to device")
+            val feedbackBatch = feedbackReceiver!!.receive(10000)
+            if (feedbackBatch != null) {
+                println(
+                    "Message feedback received, feedback time: "
+                            + feedbackBatch.enqueuedTimeUtc.toString()
+                )
+            }
+            feedbackReceiver?.close()
+            serviceClient.close()
+        }
+    }
 }
