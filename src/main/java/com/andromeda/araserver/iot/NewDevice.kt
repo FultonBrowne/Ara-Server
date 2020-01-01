6074200677c09caddb561e8101b1b62410bc9b10
fulton <gprlabs@outlook.com>
2020-01-01 13:50:54 -0800

    working on device registration

--- src/main/java/com/andromeda/araserver/iot/NewDevice.kt
+++ src/main/java/com/andromeda/araserver/iot/NewDevice.kt
@@ -1,5 +1,8 @@
 package com.andromeda.araserver.iot
 
+import com.microsoft.azure.documentdb.ConnectionPolicy
+import com.microsoft.azure.documentdb.ConsistencyLevel
+import com.microsoft.azure.documentdb.DocumentClient
 import com.microsoft.azure.sdk.iot.service.Device
 import com.microsoft.azure.sdk.iot.service.RegistryManager
 import com.microsoft.azure.sdk.iot.service.auth.AuthenticationType
@@ -7,10 +10,23 @@ import com.microsoft.azure.sdk.iot.service.auth.AuthenticationType
 
 class NewDevice {
     fun main(url: String){
+        val dbLink = System.getenv("IOTDB")
+        val client = DocumentClient("https://ara-account-data.documents.azure.com:443/", dbLink, ConnectionPolicy(), ConsistencyLevel.Session)
         val mainVal = url.replace("/newdevice/", "")
+        val actions = mainVal.split("&")
+        var id:String? = null
+        var key:String? = null
+        for (i in actions) when {
+            i.startsWith("id=") -> id = i.replace("id=", "")
+            i.startsWith("user=") -> key = i.replace("user=", "")
+            else -> throw SecurityException("not a valid set of arguments")
+        }
+        val idDB =key+id
+        DevicesDB().getDB(client, key!!, id!!)
+        val rm = RegistryManager()
+        val device = Device.createDevice( idDB, AuthenticationType.SAS)
+        rm.addDevice(device)
 
-       val rm = RegistryManager()
-        val device = Device.createDevice("", AuthenticationType.SAS)
     }
 
 }
