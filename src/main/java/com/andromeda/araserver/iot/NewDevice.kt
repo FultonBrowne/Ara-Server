df0130b8c904cfd3e22059133807f8aba2db6f7d
fulton <gprlabs@outlook.com>
2020-01-01 13:10:40 -0800

    working on stuff

--- src/main/java/com/andromeda/araserver/iot/NewDevice.kt
+++ src/main/java/com/andromeda/araserver/iot/NewDevice.kt
@@ -1,4 +1,27 @@
 package com.andromeda.araserver.iot
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
 class NewDevice {
+    fun main(){
+        val url = ""
+        val client = OkHttpClient()
+        var json = ""
+        val request = Request.Builder()
+            .url(url)
+            //.header("User-Agent", "OkHttp Headers.java")
+            .addHeader("Accept", "application/x-javascript")
+            //.addHeader("Accept", "application/vnd.github.v3+json")
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            json = response.body!!.string()
+            println(json)
+        }
+    }
+
 }
