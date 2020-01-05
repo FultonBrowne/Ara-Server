c368454929dbed3a6c96279ff126e5ad9ea1fa48
fulton <gprlabs@outlook.com>
2020-01-05 15:39:31 -0800

    stuff with classes

--- src/main/java/com/andromeda/araserver/iot/GetDeviceClass.kt
+++ src/main/java/com/andromeda/araserver/iot/GetDeviceClass.kt
@@ -1,10 +1,17 @@
 package com.andromeda.araserver.iot
 
-import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.lang.reflect.Modifier.TRANSIENT
+
 
 class GetDeviceClass {
     fun main(url:String):String{
+        val gson = GsonBuilder()
+            .excludeFieldsWithoutExposeAnnotation()
+            .excludeFieldsWithModifiers(TRANSIENT) // STATIC|TRANSIENT in the default configuration
+            .create()
+
         val mainVal = url.replace("/class/", "")
-        return Gson().toJson(TypeClassMap().main(mainVal))
+        return gson.toJson(TypeClassMap().main(mainVal))
     }
 }
