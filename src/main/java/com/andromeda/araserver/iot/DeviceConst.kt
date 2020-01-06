9e4fa2f20d68b85cb1c32cfbb8772b708eebe267
fulton <gprlabs@outlook.com>
2020-01-06 10:55:28 -0800

    more models

--- src/main/java/com/andromeda/araserver/iot/DeviceConst.kt
+++ src/main/java/com/andromeda/araserver/iot/DeviceConst.kt
@@ -3,4 +3,6 @@ package com.andromeda.araserver.iot
 object DeviceConst {
     const val LIGHT = "LIGHT"
     const val TEMP = "TEMP"
+    const val CUSTOM = "CUSTOM"
+    const val SHADES = "SHADES"
 }
