a482d98f149839b3b34e2c48268bd26fbb5ea9e4
fulton <gprlabs@outlook.com>
2020-01-09 10:42:38 -0800

    light it up (flashlight stuff)

--- src/main/java/com/andromeda/araserver/pages/GetSkillData.kt
+++ src/main/java/com/andromeda/araserver/pages/GetSkillData.kt
@@ -3,7 +3,7 @@ package com.andromeda.araserver.pages
 class GetSkillData {
     fun main(url:String): String? {
         val mainVal = url.replace("/skillsdata/", "")
-        val mainMap = mapOf("CALL" to "who should I call?", "TOG_MEDIA" to "", "SITE" to "what site?","OUTPUT" to "title and description", "TEXT" to "Who to text?", "OPEN_APP" to "what app I run?")
+        val mainMap = mapOf("CALL" to "who should I call?", "TOG_MEDIA" to "", "SITE" to "what site?","OUTPUT" to "title and description", "TEXT" to "Who to text?", "OPEN_APP" to "what app should I run?", "LIGHT" to "on or off")
         return mainMap[mainVal]
     }
 }
