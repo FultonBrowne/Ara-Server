706a458a64df6eecdf53565b1c9e55087c089ad5
fulton <gprlabs@outlook.com>
2020-01-16 13:36:30 -0800

    added hello image as a test

--- src/main/java/com/andromeda/araserver/skills/Hello.java
+++ src/main/java/com/andromeda/araserver/skills/Hello.java
@@ -14,7 +14,7 @@ public class Hello {
         Gson output = gsonBuilder.create();
         ArrayList<OutputModel> test = new ArrayList<>();
         //say hi
-        test.add(new OutputModel("hi", "hi", "hi", "hi", "Hello Human", ""));
+        test.add(new OutputModel("hi", "hi", "hi", "https://arafilestore.file.core.windows.net/ara-server-files/pablo-gentile-3MYvgsH1uK0-unsplash.jpg?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2024-04-01T22:11:11Z&st=2019-12-19T15:11:11Z&spr=https&sig=lfjMHSahA6fw8enCbx0hFTE1uAVJWvPmC4m6blVSuuo%3D", "Hello Human", ""));
         //return string value
         return output.toJson(test);
     }
