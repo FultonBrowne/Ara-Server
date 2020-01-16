ff72ac80636e2b4a007d12af480f41cc728d2260
FultonB <50185337+andromeda-software@users.noreply.github.com>
2020-01-16 09:00:16 -0800

    minor tweak

--- src/main/java/com/andromeda/araserver/util/OutputModel.java
+++ src/main/java/com/andromeda/araserver/util/OutputModel.java
@@ -8,6 +8,7 @@ public class OutputModel {
     public String description;
     public String OutputTxt;
     public String exes;
+    public String image;
 
     public OutputModel(String title, String description, String link, String image, String OutputTxt, String exes) {
         this.title = title;
@@ -15,5 +16,6 @@ public class OutputModel {
         this.description = description;
         this.OutputTxt = OutputTxt;
         this.exes = exes;
+        this.image = image;
     }
 }
