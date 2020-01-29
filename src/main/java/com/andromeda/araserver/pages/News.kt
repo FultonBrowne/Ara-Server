ed5b06db1aec06881672498e33ac32cd3e820bc2
fulton <gprlabs@outlook.com>
2020-01-29 10:58:34 -0800

    new news things

--- src/main/java/com/andromeda/araserver/pages/News.kt
+++ src/main/java/com/andromeda/araserver/pages/News.kt
@@ -2,6 +2,7 @@ package com.andromeda.araserver.pages
 
 import com.andromeda.araserver.util.NewsData
 import com.google.gson.Gson
+import java.net.URL
 
 class News{
     fun main(url:String): String? {
@@ -16,6 +17,10 @@ class News{
             news.add(NewsData(i.title, i.description.value, i.description.value, i.link, catList, i.publishedDate.time))
         }
         return Gson().toJson(news)
+
+    }
+    fun getData(newsCat:String){
+        val data = URL("https://newsapi.org/v2/top-headlines?country=us&apiKey=4e3c5ce2cfff4634b4c4811c01fd2ad1").readText()
 
     }
 }
