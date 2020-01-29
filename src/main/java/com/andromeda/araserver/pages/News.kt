fd3ccacbaf9586645d7f45d343a755b74603a024
fulton <gprlabs@outlook.com>
2020-01-29 11:13:55 -0800

    new news things

--- src/main/java/com/andromeda/araserver/pages/News.kt
+++ src/main/java/com/andromeda/araserver/pages/News.kt
@@ -2,6 +2,7 @@ package com.andromeda.araserver.pages
 
 import com.andromeda.araserver.util.NewsData
 import com.google.gson.Gson
+import com.google.gson.JsonParser
 import java.net.URL
 
 class News{
@@ -9,18 +10,17 @@ class News{
         val rssFeed = RssMain.rssMain1(0)
         val news = arrayListOf<NewsData>()
 
-        for (i in rssFeed.entries){
-            val catList = arrayListOf<String>()
-            i.categories.forEach{
-                catList.add(it.name)
-            }
-            news.add(NewsData(i.title, i.description.value, i.description.value, i.link, catList, i.publishedDate.time))
-        }
         return Gson().toJson(news)
 
     }
     fun getData(newsCat:String){
+        val newsData = arrayListOf<NewsData>()
         val data = URL("https://newsapi.org/v2/top-headlines?country=us&apiKey=4e3c5ce2cfff4634b4c4811c01fd2ad1").readText()
+        JsonParser().parse(data).asJsonObject.get("articles").asJsonArray.forEach{
+            val json = it.asJsonObject
+            newsData.add(NewsData(json.get("title").asString, json.get("content").asString, json.get("description").asString, json.get("url").asString,json.get("urlToImage").asString))
+        }
 
     }
+
 }
--- src/main/java/com/andromeda/araserver/util/NewsData.kt
+++ src/main/java/com/andromeda/araserver/util/NewsData.kt
@@ -1,2 +1,2 @@
 package com.andromeda.araserver.util
-data class NewsData(val title:String, val info:String, val smallInfo:String, val link:String, val topics:ArrayList<String>, val date:Long)
+data class NewsData(val title:String, val info:String, val smallInfo:String, val link:String, val pic:String)
