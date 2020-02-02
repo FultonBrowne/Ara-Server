447087852eab9e59fc6d8539bbd8fb29f78bfc44
fulton <gprlabs@outlook.com>
2020-02-02 08:24:06 -0800

    working on ara personality

---
+++ src/main/java/com/andromeda/araserver/persona/likesConst.kt
@@ -0,0 +1,9 @@
+package com.andromeda.araserver.persona
+
+object likesConst {
+    const val TV = 0
+    const val MOVIE = 1
+    const val BOOK = 2
+    const val CAR = 3
+    const val COLOR = 4
+}
