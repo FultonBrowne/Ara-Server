de4c43606ddb8b57e671ba315c9a6ee7fedd3e0c
fulton <gprlabs@outlook.com>
2020-02-06 09:46:38 -0800

    migration to new db config

--- src/main/java/com/andromeda/araserver/localSearchData/ReadDB.kt
+++ src/main/java/com/andromeda/araserver/localSearchData/ReadDB.kt
@@ -1,4 +1,23 @@
 package com.andromeda.araserver.localSearchData
 
+import com.microsoft.azure.documentdb.*
+import org.json.JSONObject
+
 class ReadDB {
+    fun main(client: DocumentClient): ArrayList<SkillsFromDB> {
+        val skillsFromDB = ArrayList<SkillsFromDB>()
+        val options = FeedOptions()
+        options.partitionKey = PartitionKey("readonly")
+        val queryResults: FeedResponse<Document> = client.queryDocuments("/dbs/Ara-android-database/colls/Ara-android-collection", "SELECT * FROM c ", options)
+        for (i in queryResults.queryIterator){
+            val json = i.get("document") as JSONObject
+            val model = SkillsFromDB(
+                pre = json.getString("pre"),
+                end = "",
+                action= json.getString("action")
+            )
+            skillsFromDB.add(model)
+        }
+        return skillsFromDB
+    }
 }
--- src/main/java/com/andromeda/araserver/localSearchData/SkillsFromDB.kt
+++ src/main/java/com/andromeda/araserver/localSearchData/SkillsFromDB.kt
@@ -14,7 +14,7 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.andromeda.ara.util
+package com.andromeda.araserver.localSearchData
 
 data class SkillsFromDB(val pre:String, val end:String, val action:String) {
 }
