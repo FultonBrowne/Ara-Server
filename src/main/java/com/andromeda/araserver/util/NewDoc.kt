f343a8cfb78f57ed459abb3259c11b96c1690ed0
fulton <gprlabs@outlook.com>
2020-02-08 17:09:10 -0800

    working on new documents

--- src/main/java/com/andromeda/araserver/util/NewDoc.kt
+++ src/main/java/com/andromeda/araserver/util/NewDoc.kt
@@ -1,8 +1,13 @@
 package com.andromeda.araserver.util
 
+import com.azure.data.cosmos.CosmosContainerProperties
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.microsoft.azure.cosmos.CosmosClient
+import com.microsoft.azure.cosmos.CosmosItemRequestOptions
+import com.microsoft.azure.cosmos.CosmosItemResponse
 import com.microsoft.azure.documentdb.*
+
 
 class NewDoc {
     fun main(url:String, postData:String){
@@ -28,9 +33,22 @@ class NewDoc {
         return gson.fromJson(jsontxt, object : TypeToken<Any>(){}.type)
     }
     fun newDoc(client: DocumentClient, key:String, data:Any, id:String){
+        val client = CosmosClient.create("https://ara-account-data.documents.azure.com:443/",System.getenv("IOTDB") )
+
         val options =RequestOptions()
 
         options.partitionKey = PartitionKey("user-$key")
-        client.createDocument("/dbs/Ara-android-database/colls/Ara-android-collection", Document(data, id, "user-$key"),options, false )
+        options.isPopulatePartitionKeyRangeStatistics = false
+        val document = Document(data, id, "user-$key")
+        val cosmosItemRequestOptions = CosmosItemRequestOptions(document.PartitionKey)
+        val database = client.getDatabase("")
+        val containerProperties = CosmosContainerProperties("", "/PartitionKey")
+
+
+        val container = database.getContainer("")
+
+
+        container.createItem(document, cosmosItemRequestOptions)
+        //client.createDocument("/dbs/Ara-android-database/colls/Ara-android-collection", document,options, true )
     }
 }
