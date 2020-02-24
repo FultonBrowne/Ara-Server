9bea14ca6a6e90100d96e1777e9b11e82a2e7ed1
fulton <gprlabs@outlook.com>
2020-02-24 12:26:24 -0800

    HelloTest.kt done

---
+++ src/test/java/com/andromeda/araserver/test/HelloTest.kt
@@ -0,0 +1,12 @@
+package com.andromeda.araserver.test
+
+import com.andromeda.araserver.skills.Hello
+import org.junit.Test
+
+class HelloTest {
+    @Test
+    fun test(){
+        val hello = Hello()
+        hello.hello()
+    }
+}
