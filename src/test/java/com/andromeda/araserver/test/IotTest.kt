1c065f0b574679ae90ab1a418fc73c02409be807
fulton <gprlabs@outlook.com>
2020-02-25 15:21:57 -0800

    working on iot test

--- src/test/java/com/andromeda/araserver/test/IotTest.kt
+++ src/test/java/com/andromeda/araserver/test/IotTest.kt
@@ -1,14 +1,27 @@
 package com.andromeda.araserver.test
 
+import com.andromeda.araserver.iot.LightStatusModel
 import com.andromeda.araserver.iot.NewDevice
+import com.andromeda.araserver.util.NewDoc
 import org.junit.Test
 
 class IotTest {
     @Test
     fun test(){
+        if (!System.getenv().contains("IOTDB")) return
         val time = System.currentTimeMillis()
         val newIot = NewDevice()
+        val newDoc = NewDoc()
+        val id = "test$time"
+        newDoc.newDoc("test", LightStatusModel(true, null, null), id)
+        newIot.main("/newdevice/id=$id&user=test")
+        val devices =
 
-        newIot.main("/newdevice/id=test$time&user=test")
+
+
+
+
+
+
     }
 }
