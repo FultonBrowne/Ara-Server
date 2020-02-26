37844095a0ecda11ea3058e1edc4fd1b8b7cfe11
fulton <gprlabs@outlook.com>
2020-02-25 17:22:11 -0800

    working on iot test

--- src/test/java/com/andromeda/araserver/test/IotTest.kt
+++ src/test/java/com/andromeda/araserver/test/IotTest.kt
@@ -3,7 +3,9 @@ package com.andromeda.araserver.test
 import com.andromeda.araserver.iot.LightStatusModel
 import com.andromeda.araserver.iot.NewDevice
 import com.andromeda.araserver.util.NewDoc
+import com.andromeda.araserver.util.DeviceModel
 import org.junit.Test
+import com.andromeda.araserver.iot.Main
 
 class IotTest {
     @Test
@@ -13,13 +15,10 @@ class IotTest {
         val newIot = NewDevice()
         val newDoc = NewDoc()
         val id = "test$time"
-        newDoc.newDoc("test", LightStatusModel(true, null, null), id)
+        newDoc.newDoc("test", DeviceModel(id,"LIGHT", "---\\n- \\\"on\\\": true\\n  powerLevel: null\\n  color: null\\n\"", "unit test"), id)
         newIot.main("/newdevice/id=$id&user=test")
-        val devices =
-
-
-
-
+        val main = Main()
+        main.main("/devices/id=$id&user=test&run=on:true")
 
 
 
