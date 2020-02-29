8f4059f3cc0be00b39b3d72d2037a3e83ddea554
fulton <gprlabs@outlook.com>
2020-02-29 07:52:00 -0800

    working on security

--- src/main/java/com/andromeda/araserver/util/SecurityDBCheck.kt
+++ src/main/java/com/andromeda/araserver/util/SecurityDBCheck.kt
@@ -1,24 +1,39 @@
 package com.andromeda.araserver.util
 
-import java.net.URL
+import com.microsoft.graph.models.extensions.IGraphServiceClient
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.util.*
+
 
 class SecurityDBCheck {
     init {
-        update()
+
         val timer = Timer()
         val tt = object : TimerTask(){
             override fun run() {
-                val url = URL("")
-                val json = url.readText()
-                println(json)
-                update()
+
             }
 
         }
         timer.schedule(tt, 0, 30000)
     }
-    private fun update(){
-
+    private fun update(graphClient: IGraphServiceClient) {
+        val client = OkHttpClient().newBuilder()
+            .build()
+        val request: Request = Request.Builder()
+            .url("https://login.microsoftonline.com/AraLogin.onmicrosoft.com/oauth2/token")
+            .method("GET", null)
+            .addHeader("grant_type", "client_credentials")
+            .addHeader("client_id", "c6063f12-fa37-47bc-aa5d-604e60d197c2")
+            .addHeader("client_secret", System.getenv("CLIENTID"))
+            .addHeader("resource", "https://graph.windows.net/")
+            .addHeader(
+                "Content-Type",
+                "multipart/form-data; boundary=--------------------------403173814465625970221678"
+            )
+            .build()
+        val response: Response = client.newCall(request).execute()
     }
 }
