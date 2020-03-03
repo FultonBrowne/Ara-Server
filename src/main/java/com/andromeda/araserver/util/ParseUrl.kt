3f22116d1dea76fc51e1de8290dfe20a6d27eba6
fulton <gprlabs@outlook.com>
2020-03-02 19:47:24 -0800

    added location

--- src/main/java/com/andromeda/araserver/util/ParseUrl.kt
+++ src/main/java/com/andromeda/araserver/util/ParseUrl.kt
@@ -1,20 +1,23 @@
 package com.andromeda.araserver.util
 
+import java.util.*
+
 class ParseUrl {
     fun parseApi(url: String, toReplace:String): ApiParams {
         val split = list(url, toReplace)
         var loc = ""
         var lat = ""
+        var cc = Locale.US
         var term = ""
         split.forEach{
             when {
                 it.startsWith("log") -> loc = it.replace("loc=", "")
                 it.startsWith("lat") -> lat= it.replace("lat=", "")
+                it.startsWith("cc") -> cc= Locale(it.replace("cc=", ""))
                 else -> term = it
             }
         }
-        return ApiParams(term, loc, lat)
-
+        return ApiParams(term, loc, lat, cc)
     }
 
     private fun list(url: String, toReplace: String): List<String> {
@@ -22,5 +25,5 @@ class ParseUrl {
         val split = mainVal.split("&")
         return split
     }
-    data class ApiParams(val term:String, val loc:String, val lat:String)
+    data class ApiParams(val term:String, val loc:String, val lat:String,val  cc:Locale)
 }
