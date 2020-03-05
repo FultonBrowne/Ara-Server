0d9818b5644765e7e5e214c01dd11d0a4de10109
fulton <gprlabs@outlook.com>
2020-03-05 15:36:35 -0800

    working on test : Port.kt

---
+++ src/test/java/com/andromeda/araserver/test/TestPort.kt
@@ -0,0 +1,11 @@
+package com.andromeda.araserver.test
+
+import com.andromeda.araserver.util.Port
+import org.junit.Test
+
+class TestPort {
+    @Test
+    fun test(){
+        Port().main()
+    }
+}
