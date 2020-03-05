0ffdc78d147c663023da69138aaafd1e49238af2
fulton <gprlabs@outlook.com>
2020-03-04 16:37:44 -0800

    working on tests

--- src/test/java/com/andromeda/araserver/test/ListDevicesTest.kt
+++ src/test/java/com/andromeda/araserver/test/ListDevicesTest.kt
@@ -1,4 +1,12 @@
 package com.andromeda.araserver.test
 
+import com.andromeda.araserver.iot.ListDevices
+import org.junit.Test
+
 class ListDevicesTest {
+    @Test
+    fun test(){
+        if (!System.getenv().contains("IOTDB")) return
+        ListDevices().main("/test/")
+    }
 }
