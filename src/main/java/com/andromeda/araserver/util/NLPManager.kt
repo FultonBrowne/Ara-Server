5e909bdf7431225a1a68496560f60113166f635a
fulton <gprlabs@outlook.com>
2020-03-11 15:09:48 -0700

    working on locale

--- src/main/java/com/andromeda/araserver/util/NLPManager.kt
+++ src/main/java/com/andromeda/araserver/util/NLPManager.kt
@@ -4,11 +4,13 @@ import opennlp.tools.parser.Parser
 import opennlp.tools.parser.ParserFactory
 import opennlp.tools.parser.ParserModel
 import java.net.URL
-import java.util.*
 
 class NLPManager {
     init {
-        download("https://arafilestore.file.core.windows.net/ara-server-files/parse.bin?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2024-04-01T22:11:11Z&st=2019-12-19T15:11:11Z&spr=https&sig=lfjMHSahA6fw8enCbx0hFTE1uAVJWvPmC4m6blVSuuo%3D", LanguageConst.en)
+        download(
+            "https://arafilestore.file.core.windows.net/ara-server-files/parse.bin?sv=2019-02-02&ss=bfqt&srt=sco&sp=rwdlacup&se=2024-04-01T22:11:11Z&st=2019-12-19T15:11:11Z&spr=https&sig=lfjMHSahA6fw8enCbx0hFTE1uAVJWvPmC4m6blVSuuo%3D",
+            LanguageConst.en
+        )
     }
     fun download(link:String, int: Int){
         val url =
