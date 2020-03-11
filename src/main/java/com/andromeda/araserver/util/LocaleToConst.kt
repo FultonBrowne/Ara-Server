1216b6b6bdf6e974de5d3b890aaed670ea5154f1
fulton <gprlabs@outlook.com>
2020-03-11 14:53:21 -0700

    working on locale

--- src/main/java/com/andromeda/araserver/util/LocaleToConst.kt
+++ src/main/java/com/andromeda/araserver/util/LocaleToConst.kt
@@ -3,7 +3,8 @@ package com.andromeda.araserver.util
 import java.util.*
 
 class LocaleToConst {
-    fun main(locale: Locale){
-
+    fun main(locale: Locale): Int {
+        val map = mapOf(Locale.CANADA to LanguageConst.en, Locale.US to LanguageConst.en, Locale.UK to LanguageConst.en)
+        return map.getOrDefault(locale, LanguageConst.en)
     }
 }
