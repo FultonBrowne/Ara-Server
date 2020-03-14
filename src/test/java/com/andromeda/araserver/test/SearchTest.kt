9338238ace8c4ac06464909c2c24c7fbfa89dd87
fulton <gprlabs@outlook.com>
2020-03-14 13:00:46 -0700

    search tests

--- src/test/java/com/andromeda/araserver/test/SearchTest.kt
+++ src/test/java/com/andromeda/araserver/test/SearchTest.kt
@@ -1,6 +1,7 @@
 package com.andromeda.araserver.test
 
 import com.andromeda.araserver.pages.ApiStart
+import com.andromeda.araserver.skills.GetInfo
 import com.andromeda.araserver.util.KeyWord
 import com.andromeda.araserver.util.NLPManager
 import opennlp.tools.parser.ParserFactory
@@ -14,13 +15,18 @@ class SearchTest {
         if (!NLPManager.hasRun) NLPManager()
         if(!System.getenv().contains("IOTDB")) return
         val apiStart = ApiStart()
+        val getInfo = GetInfo()
         println("test")
         apiStart.apiMain("/api/hello ara&log=0.0&lat=0.0&cc=US")
         apiStart.apiMain("/api/what is pi&log=0.0&lat=0.0&cc=US")
         apiStart.apiMain("/api/microsoft&log=0.0&lat=0.0&cc=US")
-        //apiStart.apiMain("/api/start a timer for 5 minutes&log=0.0&lat=0.0&cc=US")
+        apiStart.apiMain("/api/start a timer for 5 minutes&log=0.0&lat=0.0&cc=US")
         apiStart.apiMain("/api/rem music videos&log=0.0&lat=0.0&cc=US")
         apiStart.apiMain("/api/ara voice assistant&log=0.0&lat=0.0&cc=US")
+        getInfo.getBing("/searchb/ara voice assistant&log=0.0&lat=0.0&cc=US")
+        getInfo.bingNews("/searchn/ara voice assistant&log=0.0&lat=0.0&cc=US")
+        getInfo.main("/searcht/ara voice assistant&log=0.0&lat=0.0&cc=US")
+
 
 
 
