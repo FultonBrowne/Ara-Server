b75356f906c2ddd7ccddc9654390c1cdd75e14cf
fulton <gprlabs@outlook.com>
2020-03-15 13:39:17 -0700

    working on Reminders.kt

--- src/main/java/com/andromeda/araserver/skills/Reminders.kt
+++ src/main/java/com/andromeda/araserver/skills/Reminders.kt
@@ -1,11 +1,17 @@
 package com.andromeda.araserver.skills
 
+import com.andromeda.araserver.util.LocaleToConst
 import com.andromeda.araserver.util.ParseUrl
+import com.andromeda.araserver.util.SortWords
+import java.util.*
 
 class Reminders {
     fun new(mainUrl:String): String {
         val apiParams = ParseUrl().parseApi(mainUrl, "/remindern/")
         val term = apiParams.term
+        val cc = LocaleToConst().main(apiParams.cc)
+        val sortWords = SortWords(term, cc)
+        val topics = sortWords.getTopics()
         return "ok"
     }
 }
