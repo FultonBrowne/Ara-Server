29f7acc93728b2e17882c7488fc78d40ec5fff5c
fulton <gprlabs@outlook.com>
2020-03-27 15:13:15 -0700

    edited the personality system

--- src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
+++ src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
@@ -2,34 +2,21 @@ package com.andromeda.araserver.persona
 
 import com.andromeda.araserver.util.CosmosClients
 import com.andromeda.araserver.util.OutputModel
-import com.andromeda.araserver.util.SortWords
 import com.google.gson.Gson
 import com.microsoft.azure.documentdb.FeedOptions
 import com.microsoft.azure.documentdb.PartitionKey
 import org.json.JSONObject
-import java.sql.Connection
-import java.sql.DriverManager
 
 class GetDbArray {
-    private val link = "araresdb.database.windows.net"
-    private val userName = "pholtor"
-    private val password =  System.getenv("PASSWORD");
-    private var url = String.format(
-        "jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;" + "hostNameInCertificate=*.database.windows.net;loginTimeout=30;",
-        link,
-        "ara",
-        userName,
-        password)
     fun likes(search: String): String? {
         val options = FeedOptions()
         options.partitionKey = PartitionKey("likes")
         CosmosClients.client.queryDocuments("/dbs/Ara-android-database/colls/Ara-android-collection", "SELECT * FROM c ", options).queryIterable.forEach{
             val json = it.get("document") as JSONObject
-            val level = json.getInt("level")
+            val level = json.getString("level")
             println(level)
             if (json.getString("name").contains(search)){
-                val response = Responses.main(level)!!.replace("TERM", search)
-                val outputModel =  arrayListOf(OutputModel(response, "", "", "", response, "" ))
+                val outputModel =  arrayListOf(OutputModel(search, "", "", "", level, "" ))
                 return Gson().toJson(outputModel)
             }
         }
