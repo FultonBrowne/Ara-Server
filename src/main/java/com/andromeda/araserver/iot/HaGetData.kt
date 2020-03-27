86545178da780d4746a61f185c2ba7f64b1715e3
fulton <gprlabs@outlook.com>
2020-03-26 17:19:57 -0700

    working on ha cloud sync

---
+++ src/main/java/com/andromeda/araserver/iot/HaGetData.kt
@@ -0,0 +1,13 @@
+package com.andromeda.araserver.iot
+
+import com.andromeda.araserver.localSearchData.ReadDB
+import com.andromeda.araserver.util.CosmosClients
+import com.google.gson.Gson
+
+class HaGetData {
+    fun main(url:String): String? {
+        val mainVal = url.replace("/getha/", "")
+        return Gson().toJson(ReadDB().useHaData(CosmosClients.client, mainVal))
+
+    }
+}
