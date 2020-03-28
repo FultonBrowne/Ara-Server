931937d88c809080c2510bcbe9fed3c145505567
fulton <gprlabs@outlook.com>
2020-03-28 16:01:00 -0700

    working on the ML approach

--- src/main/java/com/andromeda/araserver/persona/Main.kt
+++ src/main/java/com/andromeda/araserver/persona/Main.kt
@@ -1,9 +1,13 @@
 package com.andromeda.araserver.persona
 
+import com.andromeda.araserver.util.CosmosClients
 import com.andromeda.araserver.util.OutputModel
 import com.andromeda.araserver.util.ParseUrl
 import com.andromeda.araserver.util.SortWords
 import com.google.gson.Gson
+import com.microsoft.azure.documentdb.FeedOptions
+import com.microsoft.azure.documentdb.PartitionKey
+import org.json.JSONObject
 import java.util.ArrayList
 
 class Main {
@@ -23,7 +27,18 @@ class Main {
     }
     fun newLikes(url:String): String? {
         val params = ParseUrl().parseWordParam(url, "/likesinput/")
+        val options = FeedOptions()
         val outputModel =  arrayListOf(OutputModel("Thanks for the input", "I'll use this to form an opinion and a better understanding of the world.", "", "", "Thanks for the input", "" ))
+
+        options.partitionKey = PartitionKey("likesmodel")
+        CosmosClients.client.queryDocuments("/dbs/Ara-android-database/colls/Ara-android-collection", "SELECT * FROM c ", options).queryIterable.forEach{
+            val json = it.get("document") as JSONObject
+            val level = json.getString("level")
+            println(level)
+            if (json.getString("name").contains(params.word)){
+                return Gson().toJson(outputModel)
+            }
+        }
         return Gson().toJson(outputModel)
 
     }
