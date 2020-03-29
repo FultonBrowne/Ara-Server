1babce67cce7e2e47a794306c7ce790e2ea30ac6
fulton <gprlabs@outlook.com>
2020-03-29 14:52:25 -0700

    working on the ML approach basic version done

--- src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
+++ src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
@@ -3,6 +3,7 @@ package com.andromeda.araserver.persona
 import com.andromeda.araserver.skills.SkillsModel
 import com.andromeda.araserver.util.CosmosClients
 import com.andromeda.araserver.util.OutputModel
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.google.gson.Gson
 import com.microsoft.azure.documentdb.FeedOptions
 import com.microsoft.azure.documentdb.PartitionKey
@@ -11,18 +12,27 @@ import org.json.JSONObject
 class GetDbArray {
     fun likes(search: String): String? {
         val options = FeedOptions()
+        val mapper = YAMLMapper()
+
         options.partitionKey = PartitionKey("likes")
         CosmosClients.client.queryDocuments("/dbs/Ara-android-database/colls/Ara-android-collection", "SELECT * FROM c ", options).queryIterable.forEach{
             val json = it.get("document") as JSONObject
             val level = json.getString("level")
             println(level)
-            if (json.getString("name").contains(search)){
+            if (json.getString("name").equals(search)){
                 val outputModel =  arrayListOf(OutputModel(level, "", "", "", level, "" ))
                 return Gson().toJson(outputModel)
             }
         }
         val yml = SkillsModel("RESPOND", "I don't know yet, what do you think?", "ARASERVERlikesinput/term=TERM&word=$search")
-        val outputModel =  arrayListOf(OutputModel("I don't know, what do you think?", "", "", "", "I don't know, what do you think?", "" ))
+        val outputModel =  arrayListOf(OutputModel(
+            "I don't know, what do you think?",
+            "",
+            "",
+            "",
+            "I don't know, what do you think?",
+            mapper.writeValueAsString(yml)
+        ))
         return Gson().toJson(outputModel)
     }
 }
