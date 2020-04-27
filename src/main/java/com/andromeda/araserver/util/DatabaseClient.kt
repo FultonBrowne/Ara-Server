ad34354b40eaf9d1a2d81d61f672d10966fb7b90
fulton <gprlabs@outlook.com>
2020-04-27 11:12:47 -0700

    working on a new DB system

--- src/main/java/com/andromeda/araserver/util/DatabaseClient.kt
+++ src/main/java/com/andromeda/araserver/util/DatabaseClient.kt
@@ -1,5 +1,18 @@
 package com.andromeda.araserver.util
 
 object DatabaseClient {
+    val address:String = System.getenv("dblink")
+    val password = System.getenv("dbpassword")
+    fun new(){
 
+    }
+    fun edit(){
+
+    }
+    fun <T>get():T{
+
+    }
+    fun <T>getAll():ArrayList<T>{
+
+    }
 }
