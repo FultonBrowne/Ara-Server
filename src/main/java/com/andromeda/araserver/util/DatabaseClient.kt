54006e0f8d19bed7afde9bca9f326fc8b3933367
fulton <gprlabs@outlook.com>
2020-04-27 15:53:33 -0700

    working on a new DB system

--- src/main/java/com/andromeda/araserver/util/DatabaseClient.kt
+++ src/main/java/com/andromeda/araserver/util/DatabaseClient.kt
@@ -1,18 +1,48 @@
 package com.andromeda.araserver.util
 
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientSettings
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
+import com.mongodb.client.MongoClients
+import com.mongodb.connection.ClusterSettings
+import com.mongodb.connection.SslSettings
+import java.util.*
+import kotlin.collections.ArrayList
+
+
 object DatabaseClient {
     val address:String = System.getenv("dblink")
     val password = System.getenv("dbpassword")
+    val dbname = System.getenv("dbname")
+    val collectionName = System.getenv("collname")
+    val cred = MongoCredential.createCredential("ara", dbname, password.toCharArray())
+    var settings = MongoClientSettings.builder()
+        .credential(cred)
+        .applyToSslSettings { builder: SslSettings.Builder ->
+            builder.enabled(
+                true
+            )
+        }
+        .applyToClusterSettings { builder: ClusterSettings.Builder ->
+            builder.hosts(
+                Arrays.asList(ServerAddress(address))
+            )
+        }
+        .build()
+    val client = MongoClients.create(settings)
+    val database = client.getDatabase(dbname)
+    val collection = database.getCollection(collectionName)
     fun new(){
 
     }
     fun edit(){
+    }
+    fun <T>get(userId:String, id:String):T{
 
     }
-    fun <T>get():T{
+    fun <T>getAll(userId:String):ArrayList<T>{
 
     }
-    fun <T>getAll():ArrayList<T>{
 
-    }
 }
