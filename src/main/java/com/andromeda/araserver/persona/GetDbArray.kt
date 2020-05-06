a1dbd93d69956e9dc7496dec425bf2633df97e92
fulton <gprlabs@outlook.com>
2020-05-06 15:58:58 -0700

    working on the new DB

--- src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
+++ src/main/java/com/andromeda/araserver/persona/GetDbArray.kt
@@ -1,14 +1,6 @@
 package com.andromeda.araserver.persona
 
-import com.andromeda.araserver.skills.SkillsModel
-import com.andromeda.araserver.util.CosmosClients
 import com.andromeda.araserver.util.DatabaseClient
-import com.andromeda.araserver.util.OutputModel
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
-import com.google.gson.Gson
-import com.microsoft.azure.documentdb.FeedOptions
-import com.microsoft.azure.documentdb.PartitionKey
-import org.json.JSONObject
 import java.util.*
 
 class GetDbArray {
