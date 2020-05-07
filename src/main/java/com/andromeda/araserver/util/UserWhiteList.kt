7599fad52322b9842aef11ec557171c4ed0e6274
fulton <gprlabs@outlook.com>
2020-05-07 16:55:25 -0700

    testing

--- src/main/java/com/andromeda/araserver/util/UserWhiteList.kt
+++ src/main/java/com/andromeda/araserver/util/UserWhiteList.kt
@@ -1,7 +1,7 @@
 package com.andromeda.araserver.util
 
 object UserWhiteList {
-    private val NonUsersList = listOf("test", "search", "person", "person_temp")
+    private val NonUsersList = listOf("test", "search", "person", "likes")
     var userList = arrayListOf<String>()
     fun checkOnList(data:String): Boolean {
         if (userList.contains(data) || NonUsersList.contains(data)) return true
