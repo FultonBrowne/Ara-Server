9b449b7a8ff3e293740e24aa7575dd1bb73c2ed7
fulton <gprlabs@outlook.com>
2020-05-13 16:23:11 -0700

    finally adding "enhanced ara services"

---
+++ src/main/java/com/andromeda/araserver/util/ContactSearch.kt
@@ -0,0 +1,7 @@
+package com.andromeda.araserver.util
+//This is an OPTIONAL SERVICE I realize how this service may invade user privacy. though no data is stored this will be optional for the very privacy focused
+class ContactSearch {
+    fun main(phrase:String, contacts:List<UserFormat>){}
+
+}
+data class UserFormat(val firstName:String, val lastName:String, val nickname:String?)
