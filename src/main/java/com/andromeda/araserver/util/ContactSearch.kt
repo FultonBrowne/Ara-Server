865493ba0f9838a2e99b2d8f1657b38183cf7b7a
fulton <gprlabs@outlook.com>
2020-05-14 14:40:16 -0700

    finally adding "enhanced ara services"

--- src/main/java/com/andromeda/araserver/util/ContactSearch.kt
+++ src/main/java/com/andromeda/araserver/util/ContactSearch.kt
@@ -1,8 +1,9 @@
 package com.andromeda.araserver.util
 //This is an OPTIONAL SERVICE I realize how this service may invade user privacy. though no data is stored this will be optional for the very privacy focused
 class ContactSearch {
-    fun main(phrase:String, contacts:List<UserFormat>){
-
+    fun main(phrase:String, contacts:List<UserFormat>, locale:Int){
+        val sortWords = SortWords(phrase, locale)
+        println(sortWords.getNN())
     }
 
 }
--- src/test/java/com/andromeda/araserver/test/EASTest.kt
+++ src/test/java/com/andromeda/araserver/test/EASTest.kt
@@ -1,11 +1,15 @@
 package com.andromeda.araserver.test
 
 import com.andromeda.araserver.util.ContactSearch
+import com.andromeda.araserver.util.LocaleToConst
+import com.andromeda.araserver.util.NLPManager
 import org.junit.Test
+import java.util.*
 
 class EASTest {
     @Test
     fun test(){
-        ContactSearch().main("please call bob", arrayListOf())
+        if (!NLPManager.hasRun) NLPManager()
+        ContactSearch().main("can you please call bob", arrayListOf(), LocaleToConst().main(Locale.US))
     }
 }
