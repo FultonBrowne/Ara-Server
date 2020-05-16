25c2cc0989c25ba94b3927b9bc2ab9e456fe84fd
fulton <gprlabs@outlook.com>
2020-05-15 17:14:15 -0700

    working on the NEW nlp which will allow for EAS

--- src/main/java/com/andromeda/araserver/util/Bert.kt
+++ src/main/java/com/andromeda/araserver/util/Bert.kt
@@ -1,4 +1,19 @@
 package com.andromeda.araserver.util
 
+import org.tensorflow.Graph
+import org.tensorflow.SavedModelBundle
+import org.tensorflow.TensorFlow
+
 class Bert {
+    var graph:Graph? = null
+    var hasRun = false
+    init {
+        println(TensorFlow.version())
+        val modelBundle = SavedModelBundle.load("") //TODO: put model stream here. that may be hard so figure it out
+        graph = modelBundle.graph()
+        hasRun = true
+    }
+    fun getNames(text:String){
+        println(graph!!.operation(text))
+    }
 }
