ea0ad5a4a7533c9a1b6daa111fd6709a5722797d
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-05-28 15:59:43 +0000

    json thing

--- src/main/java/com/andromeda/araserver/util/Bert.kt
+++ src/main/java/com/andromeda/araserver/util/Bert.kt
@@ -1,6 +1,8 @@
 package com.andromeda.araserver.util
 
 import java.net.URL
+import com.google.gson.JsonParser
+import com.google.gson.JsonArray
 class Bert {
 	val serverUrl:String
     init {
@@ -12,4 +14,17 @@ class Bert {
     fun getIntent(text:String){
 	    val result = URL("$serverUrl/v0/intent?input=$text").readText() 
     }
+    fun parese(text:String):ArrayList<NlpFromServer>{
+	    val toReturn = arrayListOf<NlpFromServer>()
+	    val jdata:JsonArray = JsonParser.parseString(text).getAsJsonObject().getAsJsonArray("")
+	    jdata.forEach{
+		    val jobject = it.getAsJsonObject()
+		    val type = jobject.get("type").asString
+                    val word = jobject.get("data").asString
+		    toReturn.add(NlpFromServer(type, word))
+
+	    }
+	    return toReturn
+    }
+    data class NlpFromServer(val type:String, val word:String)
 }
