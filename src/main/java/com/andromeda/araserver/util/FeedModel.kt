7a5249035275a7e1598d2a5e7b20c46c17ab308a
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-11 22:14:21 +0000

    working on the feed output

--- src/main/java/com/andromeda/araserver/util/FeedModel.kt
+++ src/main/java/com/andromeda/araserver/util/FeedModel.kt
@@ -3,6 +3,6 @@ package com.andromeda.araserver.util
 /** 
 This is the type used for the feed list
  **/
- data class FeedModel(var title: String, var description: String, var link: String, var image: String, var longText: Boolean) {
-	var color: Int? = null
+ data class FeedModel(var title: String, var description: String, var link: String, var image: String, var longText: Boolean, var color:Int?) {
+	 constructor(title:String, description:String, link:String, image:String, longText:Boolean):this(title, description, link, image, longText, null)
 }
