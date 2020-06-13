cb2223a2a701a54b9fa7bbe474357a04ebdde5f6
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-13 12:39:27 +0000

    timer stuff

--- src/main/java/com/andromeda/araserver/skills/Timer.kt
+++ src/main/java/com/andromeda/araserver/skills/Timer.kt
@@ -2,9 +2,12 @@ package com.andromeda.araserver.skills
 
 import com.andromeda.araserver.util.OutputModel
 import com.andromeda.araserver.util.SortWords
+import com.andromeda.araserver.util.FeedModel
+import com.andromeda.araserver.util.Feed
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import com.google.gson.Gson
 import java.util.ArrayList
+import java.util.Locale
 
 class Timer {
     fun main(url: String):String{
@@ -31,4 +34,20 @@ class Timer {
 
 
     }
+    fun main(term: String, cc:Locale):Feed{
+        //parse for search term
+        val words = SortWords(mainVal = term).getComplexDate()
+        println(words)
+        var numOfUnits:Int? = null
+        var unit:String? = null
+        for(i in words){
+            if(i.type == "CD") numOfUnits = i.word.toInt()
+            else unit = i.word
+        }
+        val map = mapOf("minutes" to 60 * 1000, "minute" to 60 * 1000, "hours" to 60 * 60 * 1000, "hour" to 60 * 60 * 1000, "second" to 1000, "seconds" to 1000)
+        val time = numOfUnits?.times(map[unit]!!)
+        val gson = Gson()
+        val mapper = YAMLMapper()
+        return Feed("list" ,arrayListOf(SkillsModel("TIMER", time.toString(), "")), "timer starting", arrayListOf(FeedModel("Starting a timer", "for $numOfUnits $unit")))
+}
 }
--- src/main/java/com/andromeda/araserver/util/Feed.kt
+++ src/main/java/com/andromeda/araserver/util/Feed.kt
@@ -1,3 +1,4 @@
 package com.andromeda.araserver.util
+import com.andromeda.araserver.skills.SkillsModel
 
-data class Feed(val type:String, val action:String?, val voice:String?, val feed:ArrayList<FeedModel>)
+data class Feed(val type:String, val action:ArrayList<SkillsModel>?, val voice:String?, val feed:ArrayList<FeedModel>)
