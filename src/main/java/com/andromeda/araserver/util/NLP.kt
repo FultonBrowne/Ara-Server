f60033a716e28f3e0e1e695556e40a674cba887f
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-14 14:50:07 +0000

    nlp

--- src/main/java/com/andromeda/araserver/util/NLP.kt
+++ src/main/java/com/andromeda/araserver/util/NLP.kt
@@ -1,6 +1,19 @@
 package com.andromeda.araserver.util
-
+import java.net.URL
+import org.json.*
 class NLP(val link:String){
 	fun getTimeLength(word:String, lang:String){
 	}
+
+	private fun getDataFromServer(subUrl:String, word:String, lang:String){
+		val toParse = URL("$link/v0/$subUrl?input=$word&lang=$lang").readText()
+		val json = JSONObject(toParse).getJSONArray("data")
+		json.forEach{
+			val obj = it as JSONObject	
+			val word = Words(obj.optString("data"), obj.optString("type"))
+		}
+
+
+	}
+	data class Words(val word:String, val type:String)
 }
