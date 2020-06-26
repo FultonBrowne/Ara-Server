5770e75692b20abb9b5668682494c78f9eb72fac
Fulton Browne arch laptop <gprlabs@outlook.com>
2020-06-26 16:45:44 +0000

    bug fix

--- src/main/java/com/andromeda/araserver/pages/News.kt
+++ src/main/java/com/andromeda/araserver/pages/News.kt
@@ -27,17 +27,24 @@ class News{
         NewsCache.me = getNews("https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=4e3c5ce2cfff4634b4c4811c01fd2ad1")
         NewsCache.uk = getNews("https://newsapi.org/v2/top-headlines?country=gb&apiKey=4e3c5ce2cfff4634b4c4811c01fd2ad1")
         NewsCache.de = getNews("https://newsapi.org/v2/top-headlines?country=de&apiKey=4e3c5ce2cfff4634b4c4811c01fd2ad1")
+	NewsCache.usNews = Gson().toJson(NewsCache.us)
+    NewsCache.ukNews = Gson().toJson(NewsCache.uk)
+    NewsCache.deNews = Gson().toJson(NewsCache.de)
+    NewsCache.mexNews = Gson().toJson(NewsCache.me)
+    NewsCache.tech = Gson().toJson(NewsCache.techEn)
+    NewsCache.money = Gson().toJson(NewsCache.moneyEn)
 
     }
     fun getNews(link:String): ArrayList<NewsData> {
         val newsData = arrayListOf<NewsData>()
         val data = URL(link).readText()
+	println(data)
         JsonParser().parse(data).asJsonObject.get("articles").asJsonArray.forEach{
             val json = it.asJsonObject
             try{
                 newsData.add(NewsData(json.get("title").asString, json.get("content").asString, json.get("description").asString, json.get("url").asString,json.get("urlToImage").asString))
             }
-            catch (e:Exception){}        }
+	    catch (e:Exception){e.printStackTrace()}        }
         return newsData
     }
 
