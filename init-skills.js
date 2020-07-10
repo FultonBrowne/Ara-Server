d9bc08670331b76b3553c3ec89a1d1af2a480172
I use arch btw <gprlabs@outlook.com>
2020-07-10 17:20:22 +0000

    db code

--- init-skills.js
+++ init-skills.js
@@ -13,7 +13,70 @@ ara.createUser(
    }
 );
 //add search data
-let data = []
+let data = [
+      {
+         "url": "",
+         "data":{
+            "word": "weather",
+            "type": {
+               "dep": "nsubj",
+               "pos": "NOUN",
+               "tag": "NNS",
+               "lemma": "weather"
+            }
+         }
+      },
+      {
+         "url": "",
+         "data":{
+            "word": "timer",
+            "type": {
+               "dep": "dobj",
+               "pos": "NOUN",
+               "tag": "NNS",
+               "lemma": "timer"
+            }
+         }
+      },
+      {
+         "url": "",
+         "data":{
+            "word": "call",
+            "type": {
+               "dep": "ROOT",
+               "pos": "VERB",
+               "tag": "VB",
+               "lemma": "call"
+            }
+         }
+      },
+      {
+         "url": "",
+         "data":{
+            "word": "text",
+            "type": {
+               "dep": "ROOT",
+               "pos": "NOUN",
+               "tag": "NN",
+               "lemma": "text"
+            }
+         }
+      },
+      {
+         "url": "",
+         "data":{
+            "word": "text",
+            "type": {
+               "dep": "compound",
+               "pos": "NOUN",
+               "tag": "NNS",
+               "lemma": "text"
+            }
+         }
+      }
+
+   ]
+
 for (i of data){
    let toInsert = {
       "_id": Math.round(Math.random()).toString()
