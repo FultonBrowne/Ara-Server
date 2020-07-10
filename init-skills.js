4a8ab99b0e755134c07a8666c29bf95dbcf64adc
I use arch btw <gprlabs@outlook.com>
2020-07-10 16:37:18 +0000

    user code

--- init-skills.js
+++ init-skills.js
@@ -1,2 +1,14 @@
 //add user
-db.getSiblingDB('ara').createUser({user:'ara', pwd:'thepassword', roles:[{role:'readWrite',db:'ara'}]});
+db.getSiblingDB('ara').createUser(
+   {
+      user:'ara',
+      pwd:'thepassword',
+      roles:[
+         {
+            role:'readWrite',
+            db:'ara'
+         }
+      ]
+   }
+);
+
