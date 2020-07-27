23ef80817ed7147ba960a2ba4cc418d3895e735d
I use arch btw <gprlabs@outlook.com>
2020-07-27 10:49:31 -0700

    added the texting system

--- src/main/java/com/andromeda/araserver/skills/Text.kt
+++ src/main/java/com/andromeda/araserver/skills/Text.kt
@@ -17,4 +17,18 @@ class Text {
         if(term != "") return Gson().toJson(arrayListOf(OutputModel("texting $term", "", "", "", "texting $term", "")))
         return Gson().toJson(arrayListOf(OutputModel("who would you like to text?", "", "", "", "who would you like to text?", YAMLMapper().writeValueAsString(SkillsModel("RESPOND", "who should I text?", "smsto:INPUT")))))
     }
+
+    fun main(params:ParseUrl.ApiParams):Feed{
+       var contact = NLP.baseNlp.getContact(params.term, params.cc.language)
+       if(contact.name.size == 0){
+          return Feed("list", null, "I am sorry, you must provide a phone number", arrayListOf(FeedModel("I am sorry, you must provide a phone number", "if this is a bug tap select here to report", "https://github.com/fultonbrowne/ara-android")))
+       }
+       var name = ""
+       for (i in contact.name){
+          if(name == "") name = name + i
+          else name = "$name $i"
+       }
+
+       return Feed("list", arrayListOf(SkillsModel("TEXT", name, contact.type?: "")), "texting $name", arrayListOf(FeedModel("Opening text app...", name)))
+    }
 }
