ce3d8bd94ab9e3734ee4f19a781c951f0cbaaba3
I use arch btw <gprlabs@outlook.com>
2020-07-29 14:13:20 -0700

    added call and text number NLP

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(fulton.software)
+domains=(fultontestmachine1.fulton.software)
 rsa_key_size=4096
 data_path="./keys"
 email="gprlabs@outlook.com" # Adding a valid address is strongly recommended
