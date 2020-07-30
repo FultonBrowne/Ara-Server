55eeea551a692a6bb1a93b322066d155dec77384
I use arch btw <gprlabs@outlook.com>
2020-07-30 14:25:29 -0700

    trying to fixed non ca error

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -9,7 +9,7 @@ domains="fultontestmachine1.fulton.software"
 rsa_key_size=4096
 data_path="./keys"
 email="gprlabs@outlook.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
