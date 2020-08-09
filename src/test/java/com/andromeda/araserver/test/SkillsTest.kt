779c2fbf9ea6b15ab2b45fe14a70d9ad325b28d4
I use arch btw <gprlabs@outlook.com>
2020-08-09 13:17:04 -0700

    added some test

--- src/test/java/com/andromeda/araserver/test/SkillsTest.kt
+++ src/test/java/com/andromeda/araserver/test/SkillsTest.kt
@@ -1,15 +1,21 @@
-package com.andromeda.ara.test
-
+package com.andromeda.araserver.test
+import com.andromeda.araserver.skills.*
+import com.andromeda.araserver.util.ParseUrl
+import java.util.*
+import org.junit.Test
 class SkillsTest{
+   @Test
    fun call(){
-
+      val params = ParseUrl.ApiParams("call bob", "0.0", "0.0", Locale.US, "bvbjrblvrvrkvblfkvn")
    }
-
+   @Test
    fun text(){
+      val params = ParseUrl.ApiParams("text bob", "0.0", "0.0", Locale.US, "bvbjrblvrvrkvblfkvn")
 
    }
-
+   @Test
    fun timer(){
+      val params = ParseUrl.ApiParams("set a timer for 5 minutes", "0.0", "0.0", Locale.US, "bvbjrblvrvrkvblfkvn")
 
    }
 }
