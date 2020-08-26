820bc7ac149c4eba6f340b0499d1eee5c15f5052
fultons gentoo machine <gprlabs@outlook.com>
2020-08-26 14:26:03 -0700

    fixed bug

--- src/main/java/com/andromeda/araserver/util/UserWhiteList.kt
+++ src/main/java/com/andromeda/araserver/util/UserWhiteList.kt
@@ -4,6 +4,12 @@ object UserWhiteList {
     private val NonUsersList = listOf("test", "search", "personapend", "likesmodel")
     var userList = arrayListOf<String>()
     fun checkOnList(data:String): Boolean {
+       try{
+          val ifExist = System.getenv("AUTH_NEEDED_TO_CREATE")!!
+          return true
+       }
+       catch(e:Exception){
+       }
         if (userList.contains(data) || NonUsersList.contains(data)) return true
         return false
     }
